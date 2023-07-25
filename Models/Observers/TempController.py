4fab312459644cf8aa806289600487f84273cec5
alexanderdomino <alexander.domino@gmail.com>
2023-07-25 15:00:27 +0200

    formatted BeerTempController.iml class

--- Models/Observers/TempController.py
+++ Models/Observers/TempController.py
@@ -5,8 +5,8 @@ from Interfaces.IActuator import IActuator
 from Models.Subjects.TemperatureTarget import TemperatureTarget
 from Models.Subjects.TemperatureSensor import TemperatureSensor
 
-class TempController(IObserver):
 
+class TempController(IObserver):
     _allowedHysteresis: int = 1
     _targetTemp: int = 27
 
@@ -26,42 +26,42 @@ class TempController(IObserver):
                 self.state = TempController.SETUP
                 self.cooler.Stop()
                 self.heater.Stop()
-            
+
             elif subject.state == TemperatureTarget.RUNNING:
                 self._targetTemp = subject._targetTemp
                 self._allowedHysteresis = subject._hysteresis
                 self.state = TempController.IDLE
-            
+
         elif isinstance(subject, TemperatureSensor):
             if self.state == TempController.IDLE:
                 if subject._currentTemp > (self._targetTemp + self._allowedHysteresis):
-                    #implement cooling logic
+                    # implement cooling logic
                     print("Controller: Start cooling")
                     self.cooler.Start()
                     self.state = TempController.COOLING
-                
+
                 elif subject._currentTemp < (self._targetTemp - self._allowedHysteresis):
-                    #implement heating logic
+                    # implement heating logic
                     print("Controller: Start heating")
                     self.heater.Start()
                     self.state = TempController.HEATING
 
             elif self.state == TempController.COOLING:
                 if subject._currentTemp > self._targetTemp:
-                    #continue cooling
+                    # continue cooling
                     print("Controller: Cooling in progress")
                 else:
-                    #turn off cooling
+                    # turn off cooling
                     print("Controller: Cooling complete")
                     self.cooler.Stop()
                     self.state = TempController.IDLE
 
             elif self.state == TempController.HEATING:
                 if subject._currentTemp < self._targetTemp:
-                    #continue heating
+                    # continue heating
                     print("Controller: Heating in progress")
                 else:
-                    #turn off heating
+                    # turn off heating
                     print("Controller: Heating complete")
                     self.heater.Stop()
                     self.state = TempController.IDLE
